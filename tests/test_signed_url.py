afe8a171154456cc4ddeb6ca2d38e20a50d8d955
Kreuz45 <kreuz45@kreuz45.com>
2023-08-05 21:20:15 +0900

    test: skip tests if there is a problem with credentials

--- tests/test_signed_url.py
+++ tests/test_signed_url.py
@@ -9,16 +9,22 @@ load_dotenv()  # take environment variables from .env.
 
 class TestSignedURL(unittest.TestCase):
     def test_generate_download_signed_url(self):
-        url = gcshus.generate_download_signed_url(
-            storage.Client(), os.environ["TEST_GCS_BUCKET"], "test.txt", 5
-        )
-        assert len(url) > 0
+        try:
+            url = gcshus.generate_download_signed_url(
+                storage.Client(), os.environ["TEST_GCS_BUCKET"], "test.txt", 5
+            )
+            assert len(url) > 0
+        except AttributeError as e:  # Credentials error
+            self.skipTest(e)
 
     def test_generate_download_signed_url_with_token_refresh(self):
-        url = gcshus.generate_download_signed_url_with_token_refresh(
-            storage.Client(), os.environ["TEST_GCS_BUCKET"], "test.txt", 5
-        )
-        assert len(url) > 0
+        try:
+            url = gcshus.generate_download_signed_url_with_token_refresh(
+                storage.Client(), os.environ["TEST_GCS_BUCKET"], "test.txt", 5
+            )
+            assert len(url) > 0
+        except gcshus.NotServiceAccountException:
+            self.skipTest("Not a service account")
 
 
 if __name__ == "__main__":
