f3aa753fa845c5b00daf5073b76b3d28ad889376
Kreuz45 <kreuz45@kreuz45.com>
2023-08-06 11:53:48 +0900

    chore: VERSION 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ AUTHOR_EMAIL = "kreuz45@kreuz45.com"
 URL = "https://github.com/yokoe/gcshus"
 LICENSE = "MIT"
 DOWNLOAD_URL = URL
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 PYTHON_REQUIRES = ">=3.6"
 INSTALL_REQUIRES = open("requirements.txt").read().splitlines()
 PACKAGES = ["gcshus"]
